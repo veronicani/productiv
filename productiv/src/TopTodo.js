6a6373779beaaa692099459c2a5148455c840e3e
Veronica Ni <vni1294@gmail.com>
2024-01-18 19:18:04 -0500

    Initial commit

---
+++ productiv/src/TopTodo.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+/** Shows the top todo.
+ *
+ * Props:
+ * - todos
+ *
+ * TodoApp -> TopTodo
+ */
+
+function TopTodo() {
+  //TODO: implement this -- comment code out
+  // lowest-priority # is the highest priority
+  // let top = todos.reduce(
+  //     (acc, cur) => cur.priority < acc.priority ? cur : acc, todos[0]);
+
+  return <Todo  />;
+}
+
+export default TopTodo;
