ecf9e84bccfda6dc2c6e3a7970e52ef9afc9f13e
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-18 19:44:47 -0600

    added functionality to editableToDoList

--- productiv/src/EditableTodoList.js
+++ productiv/src/EditableTodoList.js
@@ -11,13 +11,17 @@ import EditableTodo from "./EditableTodo";
  * TodoApp -> EditableTodoList -> [ EditableTodo, ... ]
  */
 
-function EditableTodoList() {
+function EditableTodoList({ todos, update, remove }) {
   return (
       <div>
-        {/*   FIXME  */}
-        <EditableTodo />
-        <EditableTodo />
-        <EditableTodo />
+        {todos.map(todo => (
+          <EditableTodo
+              key={todo.id}
+              todo={todo}
+              update={update}
+              remove={remove}
+            />
+        ))}
       </div>
   );
 }
